1b4cf732ac54f4c5e8c49edb4e14d4ef1e365a15
milersant <63181336+milersant@users.noreply.github.com>
2020-06-26 15:55:12 -0500

    Add files via upload

---
+++ js/fecha.js
@@ -0,0 +1,7 @@
+
+/*var f = new Date();
+document.write(f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear());*/
+var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+var diasSemana = new Array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado");
+var f=new Date();
+document.write(diasSemana[f.getDay()] + ", " + f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear());
